d2d3bff3ce6b0ee0e825cef316ec6cd3afbb4990
izimenco <diaser555@gmail.com>
2018-02-06 00:27:44 +0200

    Linear Probing Hash Table

---
+++ HashTable/LinearProbingHashTable.java
@@ -0,0 +1,115 @@
+package com.hashtable;
+
+
+public class LinearProbingHashTable<Key, Value> {
+    private static final int INITIAL_CAPACITY = 16;
+
+    private int n;           // number of key-value pairs in the symbol table
+    private int m;           // size of linear probing table
+    private Key[] keys;      // the keys
+    private Value[] vals;    // the values
+
+    public LinearProbingHashTable() {
+        this(INITIAL_CAPACITY);
+    }
+
+    public LinearProbingHashTable(int capacity) {
+        m = capacity;
+        n = 0;
+        keys = (Key[]) new Object[m];
+        vals = (Value[]) new Object[m];
+    }
+
+    public int size() {
+        return n;
+    }
+
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    public boolean contains(Key key) {
+        if (key == null) throw new IllegalArgumentException("argument to contains() is null");
+        return get(key) != null;
+    }
+
+    private int hash(Key key) {
+        return (key.hashCode() & 0x7fffffff) % m;
+    }
+
+    private void resize(int capacity) {
+        LinearProbingHashTable<Key, Value> temp = new LinearProbingHashTable<>(capacity);
+        for (int i = 0; i < m; i++) {
+            if (keys[i] != null) {
+                temp.put(keys[i], vals[i]);
+            }
+        }
+        keys = temp.keys;
+        vals = temp.vals;
+        m = temp.m;
+    }
+
+    public void put(Key key, Value val) {
+        if (key == null) throw new IllegalArgumentException("first argument to put() is null");
+
+        if (val == null) {
+            delete(key);
+            return;
+        }
+
+        // double table size if 50% full
+        if (n >= m / 2) resize(2 * m);
+
+        int i;
+        for (i = hash(key); keys[i] != null; i = (i + 1) % m) {
+            if (keys[i].equals(key)) {
+                vals[i] = val;
+                return;
+            }
+        }
+        keys[i] = key;
+        vals[i] = val;
+        n++;
+    }
+
+    public Value get(Key key) {
+        if (key == null) throw new IllegalArgumentException("argument is null");
+        for (int i = hash(key); keys[i] != null; i = (i + 1) % m)
+            if (keys[i].equals(key))
+                return vals[i];
+        return null;
+    }
+
+    public void delete(Key key) {
+        if (key == null) throw new IllegalArgumentException("argument is null");
+        if (!contains(key)) return;
+
+        // find position i of key
+        int i = hash(key);
+        while (!key.equals(keys[i])) {
+            i = (i + 1) % m;
+        }
+
+        // delete key and associated value
+        keys[i] = null;
+        vals[i] = null;
+
+        // rehash all keys in same cluster
+        i = (i + 1) % m;
+        while (keys[i] != null) {
+            // delete keys[i] an vals[i] and reinsert
+            Key keyToRehash = keys[i];
+            Value valToRehash = vals[i];
+            keys[i] = null;
+            vals[i] = null;
+            n--;
+            put(keyToRehash, valToRehash);
+            i = (i + 1) % m;
+        }
+
+        n--;
+
+        // halves size of array if it's 12.5% full or less
+        if (n > 0 && n <= m / 8) resize(m / 2);
+    }
+}
